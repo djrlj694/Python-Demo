f173a7deb0191c2348e562752a096d176a2cfa81
djrlj694 <djrlj694@users.noreply.github.com>
2017-03-15 08:23:55 -0400

    Update name.py
    Added comments and unit test.

--- name.py
+++ name.py
@@ -10,14 +10,41 @@
 class Name(object):
 
     def __init__(self, first, last):
+        """ Constructor for the class 'Name'.
+        
+        Args:
+            first (str): A person's first name.
+            last (str): A person's last name.
+        
+        Returns (Name):
+            An instance of 'Name'.
+        """
+            
         self.first = first
         self.last = last
 
     def full(self):
+        """ Returns the full name of a person.
+            
+        Args:
+            N/A
+            
+        Returns (str):
+            A person's full name.
+        """
         # NOTE: String technique #1: concatentation
         return self.first + ' ' + self.last
         
     def formal(self, prefix=''):
+        """ Returns a person's formal name.
+            
+        Args:
+            N/A
+            
+        Returns (str):
+            A person's formal name.
+        """
+        
         if len(prefix):
             # NOTE: String technique #2: templating
             return '{} {}'.format(prefix, self.full())
@@ -25,6 +52,15 @@ class Name(object):
             return self.full()
 
     def formal_from_demographics(self, sex='M', is_married=True):
+        """ Returns the formal name of a person, based on his/her demographics.
+            
+        Args:
+            N/A
+            
+        Returns:
+            A person's formal name.
+        """
+        
         prefix = ''
 
         # NOTE: Built-in function 'upper()' uppercases all of a string's letters.
@@ -39,3 +75,6 @@ class Name(object):
 
         return self.formal(prefix)
 
+# NOTE: For unit testing purposes, execute as self-test only if being run as a top-level script.
+if __name__ == "__main__":
+    pass
